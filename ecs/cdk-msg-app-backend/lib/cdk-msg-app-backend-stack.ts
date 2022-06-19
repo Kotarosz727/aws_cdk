3d9978b43e0a60f62f7c152dabe4ec24eda41b22
kotarosz727 <kotarosz727@gmail.com>
2022-06-19 20:35:06 +0900

    add

--- ecs/cdk-msg-app-backend/lib/cdk-msg-app-backend-stack.ts
+++ ecs/cdk-msg-app-backend/lib/cdk-msg-app-backend-stack.ts
@@ -1,16 +1,207 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Duration } from 'aws-cdk-lib';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class CdkMsgAppBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // dynamodb
+    const table = new dynamodb.Table(this, 'Messages', {
+      partitionKey: {
+        name: 'app_id',
+        type: dynamodb.AttributeType.STRING
+      }, 
+      sortKey: {
+        name: 'created_at',
+        type: dynamodb.AttributeType.NUMBER
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
+    new CfnOutput(this, 'TableName', { value: table.tableName });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkMsgAppBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //vpc
+    const vpc = new ec2.Vpc(this, "workshop-vpc", {
+      cidr: "10.1.0.0/16",
+      natGateways: 1,
+      subnetConfiguration: [
+        {  cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC, name: "Public" },
+        {  cidrMask: 24, subnetType: ec2.SubnetType.PRIVATE, name: "Private" }
+        ],
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    //ecr
+    const repository = new ecr.Repository(this, "workshop-api", {
+      repositoryName: "workshop-api"
+    });
+
+    //cluster
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    const executionRolePolicy =  new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ]
+    });
+    
+    // task definition
+    const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'ApiTaskDefinition', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+    fargateTaskDefinition.addToExecutionRolePolicy(executionRolePolicy);
+    fargateTaskDefinition.addToTaskRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [table.tableArn],
+      actions: ['dynamodb:*']
+    }));
+    
+    const container = fargateTaskDefinition.addContainer("backend", {
+      // Use an image from Amazon ECR
+      image: ecs.ContainerImage.fromRegistry(repository.repositoryUri),
+      logging: ecs.LogDrivers.awsLogs({streamPrefix: 'workshop-api'}),
+      environment: { 
+        'DYNAMODB_MESSAGES_TABLE': table.tableName,
+        'APP_ID' : 'my-app'
+      }
+    });
+    
+    container.addPortMappings({
+      containerPort: 3000
+    });
+
+    //service
+    const sg_service = new ec2.SecurityGroup(this, 'MySGService', { vpc: vpc });
+    sg_service.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(3000));
+
+    const service = new ecs.FargateService(this, 'Service', {
+      cluster,
+      taskDefinition: fargateTaskDefinition,
+      desiredCount: 2,
+      assignPublicIp: false,
+      securityGroups: [sg_service]
+    });
+    
+    // Setup AutoScaling policy
+    const scaling = service.autoScaleTaskCount({ maxCapacity: 6, minCapacity: 2 });
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: Duration.seconds(60),
+      scaleOutCooldown: Duration.seconds(60)
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [service],
+      healthCheck: { path: '/api/' }
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    //code commit repository
+    const code = new codecommit.Repository(this, 'Repository' ,{
+      repositoryName: 'msg-app-backend',
+      description: 'Node.js backend.', // optional property
+    });
+
+
+    //code pipeline
+    const project = new codebuild.PipelineProject(this, 'MyProject',{
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+        privileged: true
+      },
+    });
+    const buildRolePolicy =  new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:PutImage"
+            ]
+    });
+    project.addToRolePolicy(buildRolePolicy);
+    
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
+      actionName: 'CodeCommit',
+      repository: code,
+      output: sourceOutput,
+    });
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'CodeBuild',
+      project,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+    
+    new codepipeline.Pipeline(this, 'MyPipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'Build',
+          actions: [buildAction],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+            new codepipeline_actions.EcsDeployAction({
+              actionName: "ECS-Service",
+              service: service, 
+              input: buildOutput
+            }
+            )
+          ]
+        }
+      ],
+    });
+
+
   }
 }
